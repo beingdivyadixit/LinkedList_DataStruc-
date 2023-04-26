331a51af07fef49ad05c1f917b9b23863ba8827f
Divya <beingdivyadixit@gmail.com>
2023-04-26 08:15:56 +0530

    Linklist

---
+++ LinkList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            linkedList list = new linkedList();
+            list.AddFirst(70);
+            list.AddFirst(30);
+            list.AddFirst(56);
+            list.Display();
+        }
+    }
+}
